9ed5a2b164201ed729f42caf3b3fa7c38dac4e75
Somsurya Nanda <somsuryananda@gmail.com>
2025-03-19 01:14:17 +0530

    Base working BST code

--- Trimester-2/BinarySearchTree.cpp
+++ Trimester-2/BinarySearchTree.cpp
@@ -16,66 +16,34 @@ class TreeNode
 
     TreeNode(int data)
     {
-        TreeNode* newNode = new TreeNode();
-        newNode->data = data;        
+        this->data = data;
+        left=NULL;
+        right=NULL;
     }
 };
 
 
-
-class BST
-{
-    private:
+class BST {
+private:
     TreeNode* bstroot;
     int size;
-
-    public:
-    BST()
+public:
+    BST() {
+        bstroot = NULL;
+        size = 0;
+    }
+    ~BST()
     {
-        bstroot=NULL;
-        size=0;
+        destroyTree(bstroot);
     }
 
-    TreeNode* getRoot()
-    {
+    TreeNode* getRoot() {
         return bstroot;
     }
-
-    TreeNode* insertNode(int x)
-    {
-        bstroot = insertNode(bstroot,x);
+    TreeNode* insertNode(int x) {
+        bstroot = insertNode(bstroot, x);
         return bstroot;
     }
-
-    // TreeNode* insertNode(TreeNode* root,int x)
-    // {
-    //     // Base case : root is null or found a leaf node
-    //     cout<<"50:Function entry ! "<<endl;
-    //     if(root==NULL)
-    //     {
-    //         root=new TreeNode(x);
-    //         cout<<"54:root is NULL !"<<endl;            
-    //     }
-    //     else
-    //     {
-    //         cout<<"57:root is present ! "<<endl;
-
-    //         if(x < root->data)    
-    //         {
-    //             cout<<"66:left part !"<<endl;
-    //             root->left = insertNode(root->left,x);                
-    //         }    
-    //         else if(x > root->data)
-    //         {
-    //             cout<<"70:right part !"<<endl;
-    //             root->right = insertNode(root->right,x);                
-    //         }                
-    //     }
-    //     cout<<"74:Function before return !"<<endl;
-    //     return root;
-    // }
-
-    
     TreeNode* insertNode(TreeNode* root, int x) {
         if (root == NULL) {
             root = new TreeNode(x);
@@ -88,30 +56,32 @@ class BST
         }
         return root;
     }
-
-    bool searchRecursive(TreeNode* root,int x)
-    {
-        if(root==NULL)
+    bool searchRecursive(TreeNode* root, int x) {
+        if (root == NULL)
             return false;
-
-        if(x < root->data) 
-            return searchRecursive(root->left,x);
+        if (x == root->data)
+            return true;
+        if (x < root->data)
+            return searchRecursive(root->left, x);
         else
-            return searchRecursive(root->right,x);
+            return searchRecursive(root->right, x);
     }
-
-    int searchMax(TreeNode* root)
-    {
+    int searchMax(TreeNode* root) {
         TreeNode* dummy = root;
-
-        if(dummy==NULL)
+        if (dummy == NULL)
             return -1;
-
-        while(dummy->right!=NULL)
-        {
-            dummy=dummy->right;
+        while (dummy->right != NULL) {
+            dummy = dummy->right;
         }
         return dummy->data;
+    }
+    
+    void destroyTree(TreeNode* root) {
+        if (root == NULL)
+            return;
+        destroyTree(root->left);
+        destroyTree(root->right);
+        delete root;
     }
 };
 
@@ -120,14 +90,14 @@ int main()
     BST bst;
     bst.insertNode(18);
     bst.insertNode(6);
-    // bst.insertNode(20);
-    // bst.insertNode(19);
-    // bst.insertNode(2);
-    // bst.insertNode(8);
+    bst.insertNode(20);
+    bst.insertNode(19);
+    bst.insertNode(2);
+    bst.insertNode(8);
 
-    //cout<<"Searching Key 6 :  "<<bst.searchRecursive(bst.getRoot(),6)<<endl;
+    cout<<"Searching Key 6 :  "<<bst.searchRecursive(bst.getRoot(),6)<<endl;
 
-    //cout<<"Max value : "<<bst.searchMax(bst.getRoot());
+    cout<<"Max value : "<<bst.searchMax(bst.getRoot());
     return 0;
 }
 
